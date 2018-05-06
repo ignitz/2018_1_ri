3fe11828a86d4b2564d508297678377d85b71929
ignitz <ignitzhjfk@gmail.com>
2018-05-06 19:08:51 -0300

    utils.h

---
+++ tp2/utils.h
@@ -0,0 +1,14 @@
+// Colorir letras. Testado apenas no Linux.
+#ifndef COLORS
+#define COLORS
+
+#define CYAN "\033[95m"
+#define BLUE "\033[94m"
+#define GREEN "\033[92m"
+#define WARNING "\033[93m"
+#define FAIL "\033[91m"
+#define ENDC "\033[0m"
+#define BOLD "\033[1m"
+#define UNDERLINE "\033[4m"
+
+#endif
