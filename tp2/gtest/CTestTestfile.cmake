5b864efac49b9b1f876c45ffeb2ff8ba62f2d9d0
ignitz <ignitzhjfk@gmail.com>
2018-05-06 19:10:22 -0300

    remove other packages

--- tp2/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuri/src/ufmg/2018_1_ri/tp2/googletest/googletest
-# Build directory: /home/yuri/src/ufmg/2018_1_ri/tp2/googletest/build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
